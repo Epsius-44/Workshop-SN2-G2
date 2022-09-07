56d0ef83ebdcb0eca78038cdf3917cd859a2064e
livrophage <tom.chanson@epsi.fr>
2022-09-07 12:20:05 +0200

    amélioration de la page 404

--- wp-content/themes/workshop-psn2/404.php
+++ wp-content/themes/workshop-psn2/404.php
@@ -10,7 +10,7 @@ get_header();
         <img src="<?php echo get_template_directory_uri() . '/assets/images/random.jpg' ?>" alt="">
     </div>
     <div class="text-404 center">
-        <span>Oups! Il semblerait que cette page n'existe pas <a href="">Retourner à la page d'accueil ?</a></span>
+        <span>Oups! Il semblerait que cette page n'existe pas 😭. <a href="<?php echo get_home_url() ?>">Retourner à la page d'accueil ?</a></span>
     </div>
 </div>
 </div>
