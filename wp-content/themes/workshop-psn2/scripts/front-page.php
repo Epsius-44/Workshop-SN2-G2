71cfd00fb689c1a6c6dc7d199d12423ce6ef9a91
Nolwenn213 <112861056+Nolwenn213@users.noreply.github.com>
2022-09-08 09:39:47 +0200

    Add files via upload

---
+++ wp-content/themes/workshop-psn2/scripts/front-page.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="utf-8">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+<?php wp_head() ?>
+</head>
+<body>
+
+    <header>
+    <nav class="navbar navbar-expand-lg bg-white">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="../../../images/logo3.png" alt="" width="170"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        
+        <a class="nav-link text-dark active" aria-current="page" href="#">Accueil</a>
+        <a class="nav-link text-dark" href="#">Conseil</a>
+        <a class="nav-link text-dark" href="#">Detecter</a>
+        <a class="nav-link text-dark" href="#">Single Post</a>
+        
+      </div>
+    </div>
+  </div>
+</nav>
+    </header>
+
+    
+        <section class="bienvenue">
+            <center>
+                <br>
+                <br>
+                <br>
+                <br>
+                <h1> 
+                    Bienvenue sur Fake News Academy
+                </h1>
+                <br>
+                <br>
+                <br>
+                <br>
+            </center>
+        </section>
+
+        <section class="exemple">
+        <center>
+        <br>
+        <br>
+        <br>
+        
+            
+            <FONT face="New Century Schoolbook">
+                <h2> Exemple de fake news </h2>
+            </FONT>
+            <img src="images/tweet_fake.png" width="300px" style="margin-right:5px">
+            <img src="images/fauxpostmacron.png" width="300px" style="margin-left:20px ">
+        </center>
+        
+        </section>
+        <section class="bouton">
+        <br>
+        <br>
+        <br>
+        <button type="button" class="btn btn-white btn-white"style="margin-left:200px">Conseil</button >
+        <button type="button" class="btn btn-white btn-white"style="margin-left:300px">Detecter</button>
+        <button type="button" class="btn btn-white btn-white" style="margin-left:300px">Outils</button>
+        <br>
+        <br>
+        <br>
+
+        </section>
+
+    
+
+</body>
+</html>                            
