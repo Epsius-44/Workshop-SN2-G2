a07d47ba7b659475c2fc95a0b32fc225494ad277
Valdorion <valdorn2@gmail.com>
2022-09-08 10:32:27 +0200

    ajout de l'action

--- wp-content/themes/workshop-psn2/page-search.php
+++ wp-content/themes/workshop-psn2/page-search.php
@@ -4,7 +4,7 @@ get_header(); ?>
 <div class="container-search-result">
     <section class="search-section full-page-search">
         <div class="form-container form-center">
-            <form action="" class="form-search">
+            <form action="<?php echo get_site_url()?>/search-result" method="get" class="form-search">
                 <div class="input-container">
                     <input class="full-input-search" type="text" placeholder="Vérifier un article...">
                     <button class="full-button-search" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
