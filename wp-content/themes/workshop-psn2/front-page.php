c3bd865bb812549a8c93a8434c2691777e061c39
Louis Ducruet <louis.ducruet@epsi.fr>
2022-09-08 10:38:51 +0200

    Fix front page

--- wp-content/themes/workshop-psn2/front-page.php
+++ wp-content/themes/workshop-psn2/front-page.php
@@ -1,35 +1,5 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-<meta charset="utf-8">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-<?php wp_head() ?>
-</head>
-<body>
+<?php get_header() ?>
 
-    <header>
-    <nav class="navbar navbar-expand-lg bg-white">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src="../../../images/logo3.png" alt="" width="170"></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        
-        <a class="nav-link text-dark active" aria-current="page" href="#">Accueil</a>
-        <a class="nav-link text-dark" href="#">Conseil</a>
-        <a class="nav-link text-dark" href="#">Detecter</a>
-        <a class="nav-link text-dark" href="#">Single Post</a>
-        
-      </div>
-    </div>
-  </div>
-</nav>
-    </header>
-
-    
         <section class="bienvenue">
             <center>
                 <br>
@@ -74,7 +44,4 @@
 
         </section>
 
-    
-
-</body>
-</html>                            
+<?php get_footer(); ?>
