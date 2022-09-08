52da13ca2bd163f0c220d4f81d8c7567664400f9
Charline bch <charline.bouche@epsi.fr>
2022-09-08 11:20:06 +0200

    fix scss front-page

--- wp-content/themes/workshop-psn2/front-page.php
+++ wp-content/themes/workshop-psn2/front-page.php
@@ -1,47 +1,25 @@
 <?php get_header() ?>
 
-        <section class="bienvenue">
-            <center>
-                <br>
-                <br>
-                <br>
-                <br>
-                <h1> 
-                    Bienvenue sur Fake News Academy
-                </h1>
-                <br>
-                <br>
-                <br>
-                <br>
-            </center>
-        </section>
+    <section class="bienvenue">
+        <h1> Bienvenue sur Fake News Academy </h1>
+    </section>
 
-        <section class="exemple">
-        <center>
-        <br>
-        <br>
-        <br>
-        
-            
-            <FONT face="New Century Schoolbook">
+    <section class="exemple-fake-news">
+        <div class="container-front-page">
+            <div class="card-fake-news">
                 <h2> Exemple de fake news </h2>
-            </FONT>
-            <img src="images/tweet_fake.png" width="300px" style="margin-right:5px">
-            <img src="images/fauxpostmacron.png" width="300px" style="margin-left:20px ">
-        </center>
-        
-        </section>
-        <section class="bouton">
-        <br>
-        <br>
-        <br>
-        <button type="button" class="btn btn-white btn-white"style="margin-left:200px">Conseil</button >
-        <button type="button" class="btn btn-white btn-white"style="margin-left:300px">Detecter</button>
-        <button type="button" class="btn btn-white btn-white" style="margin-left:300px">Outils</button>
-        <br>
-        <br>
-        <br>
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/tweet_fake.png' ?>"
+                     alt="Fake Tweet">
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/fauxpostmacron.png' ?>"
+                     alt="Fake Tweet Macron">
+            </div>
+        </div>
+    </section>
 
-        </section>
+    <section class="list-bouton">
+        <button type="button" class="btn btn-primary">Conseil</button>
+        <button type="button" class="btn btn-primary">Detecter</button>
+        <button type="button" class="btn btn-primary">Outils</button>
+    </section>
 
 <?php get_footer(); ?>
