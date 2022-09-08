b4e513d2f8ffb1dcdd31669fb1bb1788beefae7b
livrophage <tom.chanson@epsi.fr>
2022-09-08 09:29:23 +0200

    fix css 404

--- wp-content/themes/workshop-psn2/404.php
+++ wp-content/themes/workshop-psn2/404.php
@@ -7,7 +7,7 @@ get_header();
         <h1>Erreur <span class="rainbow rainbow_text_animated">404</span></h1>
     </div>
     <div class="image-404 center">
-        <img src="<?php echo get_template_directory_uri() . '/assets/images/random.jpg' ?>" alt="">
+        <img class="img-404" src="<?php echo get_template_directory_uri() . '/assets/images/random.jpg' ?>" alt="">
     </div>
     <div class="text-404 center">
         <span>Oups! Il semblerait que cette page n'existe pas 😭. <a class="text-404" href="<?php echo get_home_url() ?>">Retourner à la page d'accueil ?</a></span>
@@ -15,5 +15,5 @@ get_header();
 </div>
 </div>
 <?php
-get_header();
+get_footer();
 ?>
